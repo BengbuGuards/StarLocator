496d6305db9482b46aa3aa18a6bd0353c54e2879
conmmon <3026524481@qq.com>
2024-10-08 16:05:19 +0800

    feat: now you can pick the plumb line's endpoint

--- interface/ui.js
+++ interface/ui.js
@@ -1,13 +1,21 @@
 // by BengbuGuards小流汗黄豆
-var container, canvas, canvasInst, tips, cursorCrd, inputTable;
-var movable = false;
-var text, rect;
-var map;
+var container, canvas, canvasInst, tips, cursorCrd, inputTable;		// 容器，画布，画布实例，提示，鼠标坐标，输入表格
+var movable = false;		// 是否可移动
+var text, rect;		// 文本，矩形
+var map;		// 地图
 
-var points = [], ptLabels = [], numOfPts = 0;
+var points = [], ptLabels = [], numOfPts = 0;		// 星星点，标签，星星数量
 
-var lmbDown = false, cancelOp = false;
-var isPickingCele = false;
+let linePoints = []; // 存储一对铅垂线的端点
+let lines = []; // 铅垂线
+let numLine=0; // 铅垂线数量
+let numLinePoint=0; // 铅垂线端点数量
+let lineLabels = []; // 铅垂线标签
+
+var lmbDown = false, cancelOp = false;		// 鼠标左键是否按下，是否取消操作
+
+var isPickingCele = false;		// 是否正在选择天体
+let isPickingPL = false;		// 是否正在选择铅垂线	
 
 // 页面加载完成事件
 window.onload = function () {
@@ -41,7 +49,7 @@ window.onload = function () {
 	// 画布缩放移动
 	canvas.on("mouse:down", function (e) {
 		lmbDown = true;
-		if (!isPickingCele && movable && canvas.getActiveObject() == undefined) {
+		if (!isPickingCele && !isPickingPL && movable && canvas.getActiveObject() == undefined) {
 			if (!this.panning) {
 				setCanvasCursor('grabbing');
 			}
@@ -51,6 +59,7 @@ window.onload = function () {
 	});
 	canvas.on("mouse:up", function (e) {
 		lmbDown = false;
+		// 选择天体
 		if (isPickingCele) {
 			if (cancelOp) {
 				cancelOp = false;
@@ -63,13 +72,37 @@ window.onload = function () {
 			isPickingCele = false;
 			tips.innerHTML = '';
 			setCanvasCursor('grab');
-		} else if (movable && canvas.getActiveObject() == undefined) {
+		} 
+
+		// 选择铅垂线
+		else if (isPickingPL && linePoints.length < 2) {
+			if (cancelOp){
+				cancelOp = false;
+				// 取消操作
+			}
+			else {
+				// 加入
+				let p = canvas.getPointer(e.e);
+				addPLEndpoint(p.x, p.y);
+				if (linePoints.length==2){
+					lines.push(linePoints);
+					linePoints=[];
+				}               
+			}
+			isPickingPL = false;
+			tips.innerHTML = '';
+			setCanvasCursor('grab');
+		} 
+
+		else if (movable && canvas.getActiveObject() == undefined) {
 			this.panning = false;
 			canvas.selection = true;
 			setCanvasCursor('grab');
 		}
 	});
+
 	canvas.on("mouse:move", function (e) {
+		// 选择天体
 		if (isPickingCele) {
 			if (lmbDown) {
 				cancelOp = true;
@@ -80,6 +113,19 @@ window.onload = function () {
 			}
 			setCanvasCursor('crosshair');
 		}
+
+		// 选择铅垂线
+		else if (isPickingPL) {
+			if (lmbDown) {
+				cancelOp = true;
+				canvas.selection = false;
+				tips.innerHTML = '松开鼠标取消标记。';
+				setCanvasCursor('not-allowed');
+				return;
+			}
+			setCanvasCursor('crosshair');                 
+		}
+
 		// 坐标显示
 		if (movable && e && !this.panning) {
 			let p = canvas.getPointer(e.e);
@@ -90,12 +136,16 @@ window.onload = function () {
 			canvas.relativePan(delta);
 		}
 	});
+
+	// 鼠标移出画布时清空坐标
 	canvas.on('mouse:out', function (e) {
 		if (movable && e) {
 			let p = canvas.getPointer(e.e);
 			cursorCrd.innerHTML = '';
 		}
 	})
+
+	// 用鼠标滚轮控制画布缩放
 	canvas.on('mouse:wheel', opt => {
 		if (!movable) return;
 		opt.e.preventDefault()
@@ -112,6 +162,7 @@ window.onload = function () {
 		)
 	})
 
+	// 调整画布的缩放和视图位置
 	function reZoomCanvas(rect, alignRect = false, resize = true) {
 		var scaleX = canvas.width / rect.width;
 		var scaleY = canvas.height / rect.height;
@@ -132,14 +183,24 @@ window.onload = function () {
 		else reZoomCanvas(text, true, false);
 	}
 
+	//--------------------事件绑定--------------------
+	// 重置缩放事件
 	document.getElementById('resetZoom').addEventListener('click', function () {
 		if (movable) reZoomCanvas(rect);
 	});
+	// 选择天体事件
 	document.getElementById('celePick').addEventListener('click', function () {
 		if (!movable) return;
 		isPickingCele = !isPickingCele;
 		tips.innerHTML = `${isPickingCele ? '单击要选择的天体。' : ''}`;
 	});
+	// 选择铅垂线事件
+	document.getElementById('vaniZen').addEventListener('click', function () {
+		if (!movable) return;
+		isPickingPL = !isPickingPL;
+		tips.innerHTML = `${isPickingPL ? '请依次点击要选择的铅垂线的两个端点。' : ''}`;
+	});
+	//--------------------------------------------
 
 	// 读取到文件事件
 	function onImgChange(e) {
@@ -223,6 +284,8 @@ window.onload = function () {
 			console.error('加载JSON数据时出错:', error);
 		});
 }
+
+// 添加星星点的函数
 function addStarAtPoint(x, y) {
 	// 保留两位小数
 	x = Math.round(x * 100) / 100;
@@ -280,6 +343,45 @@ function addStarAtPoint(x, y) {
 	// 加入到表格中
 	document.getElementById(`coordX${numOfPts}`).value = x;
 	document.getElementById(`coordY${numOfPts}`).value = y;
+}
+
+// 添加铅垂线端点的函数
+function addPLEndpoint(x, y){
+	// 保留两位小数
+	x = Math.round(x * 100) / 100;
+	y = Math.round(y * 100) / 100;
+
+	numLinePoint++;
+	numLine=parseInt(numLinePoint/2);
+
+	let width = 32, height = 32;
+	let point = new fabric.Path('M15 0 16 16 17 0ZM0 15 16 16 0 17ZM15 32 16 16 17 32ZM32 17 16 16 32 15Z', {
+		left: x - 16,
+		top: y - 16,
+		width: width,
+		height: height,
+		fill: '#1919E6',
+		hasControls: false,
+		id: numLinePoint // 标记其索引
+	});
+	let text = new fabric.Text('>>>', {
+		left: x + 16,
+		top: y - 10,
+		fontSize: 16,
+		fontFamily: '微软雅黑',
+		fill: '#1919E6',
+		selectable: false,
+		hoverCursor: 'grab'
+	});
+	point.on("moving", e => {
+		text.left = point.left + 32, text.top = point.top + 6;
+		text.setCoords();
+	});
+	canvas.add(point);
+	canvas.add(text);
+
+	linePoints.push(point);
+	lineLabels.push(text);
 }
 /* const input = document.getElementById('hAngle1');
 const floatingDiv = document.getElementById('hourAngleInput');

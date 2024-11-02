94db6970ebeb796f8bded41015a986365087c0a3
Yisan5772156 <45277615+Yisan5772156@users.noreply.github.com>
2024-11-03 05:26:01 +0800

    feat: 自动补全候选多余5条时使用scroll选择

--- src/interface/functions/AutoComplete.js
+++ src/interface/functions/AutoComplete.js
@@ -8,15 +8,20 @@ function autoCompleteStarName(inp) {
         var a, b, i, val = this.value;
         closeAllLists();
         if (!val) { return false; }
-        currentFocus = -1;
+        currentFocus = 0;
         a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("id", this.id + "-autocomplete");
         a.setAttribute("class", "autocomplete-items");
         this.parentNode.appendChild(a);
+        b = document.createElement("DIV");
+        b.setAttribute("class", "autocomplete-list");
+        b.setAttribute("id", this.id + "-autocomplete-list");
+        a.appendChild(b);
+        a = b;
         var autoCompleteList = [];
         for (i = 0; i < arr.length; i++) {
             /*寻找子字符串*/
-            var pos = arr[i].toUpperCase().indexOf(val.toUpperCase())
+            var pos = arr[i].toUpperCase().indexOf(val.toUpperCase());
             if (pos >= 0) {
                 autoCompleteList.push({ name: arr[i], pos: pos });
             }
@@ -39,28 +44,42 @@ function autoCompleteStarName(inp) {
         )
     });
     inp.addEventListener("keydown", function (e) {
-        var x = document.getElementById(this.id + "autocomplete-list");
+        var x = document.getElementById(this.id + "-autocomplete");
         if (x) x = x.getElementsByTagName("div");
         if (e.keyCode == 40) { //down
             e.preventDefault();
             currentFocus++;
             addActive(x);
+            fixScrolling(x);
         } else if (e.keyCode == 38) { //up
             e.preventDefault();
             currentFocus--;
             addActive(x);
+            fixScrolling(x);
         } else if (e.keyCode == 13) { //enter
             e.preventDefault();
-            if (currentFocus > -1) {
+            if (currentFocus > 0) {
                 if (x) x[currentFocus].click();
             }
         }
     });
+    function fixScrolling(x){
+        if (!x) return false;
+        var l = document.getElementsByClassName("autocomplete-list")[0];
+        var singleItemHeight = l.scrollHeight / (x.length - 1);
+        console.info((currentFocus-1)*singleItemHeight);
+        if ((currentFocus-1)*singleItemHeight >= l.scrollTop + l.clientHeight-singleItemHeight){
+            l.scrollTop = (currentFocus-1-4)*singleItemHeight;
+        }
+        if ((currentFocus-1)*singleItemHeight < l.scrollTop){
+            l.scrollTop = Math.max(0,(currentFocus-1)*singleItemHeight);
+        }
+    }
     function addActive(x) {
         if (!x) return false;
         removeActive(x);
-        if (currentFocus >= x.length) currentFocus = 0;
-        if (currentFocus < 0) currentFocus = (x.length - 1);
+        if (currentFocus >= x.length) currentFocus = 1;
+        if (currentFocus < 1) currentFocus = (x.length - 1);
         x[currentFocus].classList.add("autocomplete-active");
     }
     function removeActive(x) {

7753c2bafaaf8af0c10ae3671772f8cd584e7971
cheanus <lin.pyw@qq.com>
2024-11-10 15:56:04 +0800

    fix: 查询天体坐标API适配移动端，并且提高精度

--- src/core/AstroCoord/fetch.js
+++ src/core/AstroCoord/fetch.js
@@ -4,7 +4,7 @@ import { solarBodies } from './calc.js';
 /**
  * 根据恒星名称获取其赤经和赤纬
  * @param {string} starName 恒星名称
- * @returns {Promise<[number, number]>} 返回一个Promise对象，包含赤经和赤纬J2000
+ * @returns {Promise<[number, number]>} 返回一个Promise对象，包含J2000的赤经（时）和赤纬（度）
  */
 async function getRaDeCbyName(starName) {
     // 不查询太阳系天体
@@ -15,26 +15,15 @@ async function getRaDeCbyName(starName) {
     starName = starName.replace(/\s+/, '+');
 
     let results = await fetch(
-        'https://simbad.cds.unistra.fr/simbad/sim-id?output.format=ASCII&Ident='
-        + starName
-        + '&obj.coo2=off&obj.coo3=off&obj.coo4=off'
-        + '&obj.pmsel=off&obj.plxsel=off&obj.rvsel=off&obj.spsel=off&obj.mtsel=off'
-        + '&obj.sizesel=off&obj.fluxsel=off&obj.bibsel=off&list.bibsel=off&obj.messel=off&obj.notesel=off', {
-        method: 'GET',
-    })
-    .then(response => response.text()) // 解析响应体为JSON格式
-    .then(data => {
-        // 解析数据
-        let regrx = /Coordinates\(ICRS,ep=J2000,eq=2000\):(\s*[+-\d.]*){6}/;
-        let line = data.match(regrx)[0];
-        let parts = line.split(' ').filter(part => part !== '').slice(1, 7).map(part => parseFloat(part));
-        // 处理数据
-        if (parts[3] < 0) {
-            parts[4] *= -1;
-            parts[5] *= -1;
+        'https://simbad.u-strasbg.fr/simbad/sim-nameresolver?ident=' + starName + '&output=json&data=J&option=strict',
+        {
+            method: 'GET',
         }
-        let ra = parts[0] + parts[1] / 60 + parts[2] / 3600;
-        let dec = parts[3] + parts[4] / 60 + parts[5] / 3600;
+    )
+    .then(response => response.json()) // 解析响应体为JSON格式
+    .then(data => {
+        let ra = data[0]['ra'] / 15;
+        let dec = data[0]['dec'];
         return [ra, dec];
     })
     .catch(error => {

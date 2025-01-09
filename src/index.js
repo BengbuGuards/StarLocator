95f872894372667a9ebf23fe7f1dda81da31dbb4
cheanus <lin.pyw@qq.com>
2025-01-09 17:19:31 +0800

    chore: vue基本框架

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-/* eslint-disable no-undef */
-import './css/site.css';
 import { InteractPhoto } from './interface/classes/interact.js';
 import { EventManager } from './interface/classes/EventManager.js';
 
 let interactPhoto = new InteractPhoto();
 // eslint-disable-next-line no-unused-vars
 let eventManager = new EventManager(interactPhoto);
-
-/* eslint-enable no-undef */
--- webpack.prod.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-
-module.exports = merge(common, {
-    mode: 'production',
-});
-/* eslint-enable no-undef */
